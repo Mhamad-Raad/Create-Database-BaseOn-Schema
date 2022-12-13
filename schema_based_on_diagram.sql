99cb408c83e720634ad486321f44adba9892d125
joker69216 <hamaraad883@gmail.com>
2022-12-13 23:05:07 +0300

    add foreign keys to the structure

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -9,7 +9,8 @@ create table medical_histories(
 	id serial primary key,
 	admitted_at timestamp,
 	patient_id int,
-	status varchar(50)
+	status varchar(50),
+  FOREIGN KEY (patient_id) REFERENCES patient(id)
 )
 
 
@@ -21,7 +22,9 @@ create table treatment (
 
 create table history_treatments (
 	mh_id int,
-	treatments_id int 
+	treatments_id int,
+  FOREIGN KEY (mh_id) REFERENCES medical_histories(id),
+  FOREIGN KEY (treatments_id) REFERENCES treatment(id)
 )
 
 create table invoice_items (
@@ -30,16 +33,9 @@ create table invoice_items (
 	qunatity int, 
 	total_price decimal,
 	invoice_id int,
-	treatment_id int
-)
-
-create table invoice_items (
-	id serial primary key,
-	unit_price decimal,
-	qunatity int, 
-	total_price decimal,
-	invoice_id int,
-	treatment_id int
+	treatment_id int,
+  FOREIGN KEY (invoice_id) REFERENCES invoice(id),
+  FOREIGN KEY (treatment_id) REFERENCES treatment(id)
 )
 
 create table invoice (
@@ -47,5 +43,6 @@ create table invoice (
 	total_amount decimal,
 	generate_at timestamp,
 	payed_at timestamp,
-	medical_history_id int
+	medical_history_id int,
+  FOREIGN KEY (medical_history_id) REFERENCES medical_histories(id)
 )
